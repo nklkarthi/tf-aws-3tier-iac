e1851c5c5e985848ee7b1b51180e2702938c7884
nklkarthi <nklkarthi@gmail.com>
2024-06-09 22:42:57 +0800

    Add vpc for 3-tier app

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
 resource "random_pet" "main" {
   length = 2
 }
+
+module "vpc" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=25322b6b6be69db6cca7f167d7b0e5327156a595"
+
+  name = "vpc-3tier"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
